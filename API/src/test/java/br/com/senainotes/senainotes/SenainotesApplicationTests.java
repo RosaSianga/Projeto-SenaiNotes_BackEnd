ead945f7265a465ef9138fc29bd497308b098df1
Renata Santos <renatarhc@outlook.com>
2025-09-24 21:30:32 -0300

    Projeto Senai Notes - V1

---
+++ API/src/test/java/br/com/senainotes/senainotes/SenainotesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senainotes.senainotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenainotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
