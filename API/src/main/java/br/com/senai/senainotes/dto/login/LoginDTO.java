cf3571d03124d86da22253adda703a37d0e11b28
Ericles Moura <lucasdpl71@gmail.com>
2025-10-07 21:49:56 -0300

    Sprint 3, DEV 07/10

--- API/src/main/java/br/com/senai/senainotes/dto/login/LoginDTO.java
+++ API/src/main/java/br/com/senai/senainotes/dto/login/LoginDTO.java
@@ -1,10 +1,18 @@
 package br.com.senai.senainotes.dto.login;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class LoginDTO {
 
+    @NotBlank (message = "O e-mail não pode estar em branco.")
+    @Email (message = "O formato do e-mail é inválido.")
     private String email;
+
+    @NotBlank (message = "A senha não pode estar em branco.")
+    @Size (min = 8, message = "A senha deve ter no mínimo 8 caracteres.")
     private String senha;
 }
